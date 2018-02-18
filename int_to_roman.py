a41acea9981f8ad8ea0655a19e81e944b0af6a85
alexMAF93 <mitroi.alex93@gmail.com>
2018-02-18 21:08:08 +0200

    Class that converts integers into roman numberals

--- int_to_roman.py
+++ int_to_roman.py
@@ -3,14 +3,14 @@ class roman:
         integers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
         romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
-        roman_num = ''
+        roman_numeral = ''
         i = 0
         while number > 0:
             for _ in range(number//integers[i]):
-                roman_num += romans[i]
+                roman_numeral += romans[i]
                 number -= integers[i]
             i+=1
-        return roman_num
+        return roman_numeral
 
 
 number = int(input("Insert number: "))
