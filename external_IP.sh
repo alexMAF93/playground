fbe596ce83455c741209b5031df6c97e4dd96da3
alexMAF93 <mitroi.alex93@gmail.com>
2018-09-19 21:58:52 +0300

    now the permissions for config.php are changed to apache:apache

--- external_IP.sh
+++ external_IP.sh
@@ -11,12 +11,17 @@ NEW_IP=`curl -s http://whatismyip.akamai.com`
 if [[ ! "$OLD_IP" == "$NEW_IP" ]]
 then
 	cat $DIR/config_file | sed "s/IP_TO_BE_REPLACED/${NEW_IP}/" > $DIR/config.php
+	chown apache:apache $DIR/config.php
 	ssmtp mitroi.alex93@gmail.com <<-EOF
 	To: Alex Mitroi
 	From: Alex Mitroi
 	Subject: External IP for $HOSTNAME
 
 	The new external IP: $NEW_IP
+	
+	OwnCloud: https://${NEW_IP}:19443/owncloud
+	Webmin  : https://${NEW_IP}:19999/
+	
 
 	EOF
 
