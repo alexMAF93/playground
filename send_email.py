d3910e34213346faef83cba1054ff8e3295c71a2
alexMAF93 <mitroi.alex93@gmail.com>
2018-10-21 10:28:13 +0300

    Send emails using Python

---
+++ send_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+from email.mime.text import MIMEText
+
+
+msg = MIMEText("Test email")
+msg["Subject"] = "Testing the email module"
+msg["From"] = "centos"
+msg["To"] = "mitroi.alex93@gmail.com"
+
+
+import smtplib
+
+
+t = smtplib.SMTP("smtp.gmail.com", 587)
+t.ehlo()
+t.starttls()
+t.login("maf.alex93@gmail.com", "parolasmechera")
+
+
+t.sendmail("maf.alex93@gmail.com", "mitroi.alex93@gmail.com", msg.as_string())
+
+
+t.quit()
+
