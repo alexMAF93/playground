dac7aba10361645dace710255936cc68a12bc9e4
Alex Mitroi <mitroi.alex93@gmail.com>
2019-03-10 11:15:05 +0200

    Testing the new ssh key

--- useful_functions.py
+++ useful_functions.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 
 
+# function used to get credentials for the email account
 def get_credentials_for_email(PASSWD_FILE):
     with open(PASSWD_FILE, 'r') as f:
         return f.read().split(' ')
