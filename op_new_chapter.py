e9f38ff86568574a2367f78fc90822be7da2e791
Alex Mitroi <mitroi.alex93@gmail.com>
2019-10-06 19:36:19 +0300

    ++

--- op_new_chapter.py
+++ op_new_chapter.py
@@ -20,22 +20,25 @@ def get_manga(manga_name): # gets the first link that matches the words specifie
             link_manga = link.strip()  # we keep the first result only, since it's the latest
             return link_manga
     else:
-        print('Cannot find a link for the name you specified')
+        return False
 
 
 def parse_link(link_manga): # gets the name, chapter and the date from the link
     output = "Cannot find anything..."
     link_regex = '<a href="/r/.*right">(.*)</span>(.*</i>)*(.*)<strong>(.*)</strong>.*<em>(.*)</em></a>'
-    link_search = re.search(link_regex, link_manga, re.IGNORECASE)
-    if link_search:
-        published_date = link_search.group(1)
-        manga = link_search.group(3)
-        chapter = link_search.group(4)
-        title = link_search.group(5)
-        output = """
+    if link_manga:
+        link_search = re.search(link_regex, link_manga, re.IGNORECASE)
+        if link_search:
+            published_date = link_search.group(1)
+            manga = link_search.group(3)
+            chapter = link_search.group(4)
+            title = link_search.group(5)
+            output = """
 {}, {} - {}
 Released {}
     """.format(manga.strip(), chapter, title, published_date)
+    else:
+        output = "Nothing found"
     return output
 
 
