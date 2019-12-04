0ff942e6be788187fbc102999c399ade00c2c509
Alex Mitroi <mitroi.alex93@gmail.com>
2019-12-04 20:40:42 +0200

    updated the script

--- blinking_lights.py
+++ blinking_lights.py
@@ -3,26 +3,58 @@
 
 import time
 import RPi.GPIO as GPIO
+import datetime
+import logging
 
+
+def light_state(lightpin, state):
+    GPIO.output(lightpin, state)
+    time.sleep(1)
+
+
+def off_everything(pins):
+    for pin in pins:
+        if GPIO.input(pin):
+            light_state(pin, False)
+
+
+def is_it_off(pins):
+    for pin in pins:
+        if GPIO.input(pin):
+            return False
+    return True
+
+
+logname = '/var/tmp/instalatie.log'
+logging.basicConfig(filename=logname, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
 
 lightpin1 = 37
 lightpin2 = 26
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(lightpin1, GPIO.OUT)
-GPIO.setup(lightpin2, GPIO.OUT)
-
+dt = datetime.datetime.now()
 
 while True:
-    GPIO.output(lightpin1, True)
-    time.sleep(1)
-    GPIO.output(lightpin2, True)
-    time.sleep(1)
+    if dt.time() < datetime.time(23) and dt.time() > datetime.time(18):
+        if not GPIO.getmode():
+            logging.info('Dam drumu\' la instalatie')
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(lightpin1, GPIO.OUT)
+            GPIO.setup(lightpin2, GPIO.OUT)
 
-    GPIO.output(lightpin1, False)
-    time.sleep(1)
-    GPIO.output(lightpin2, False)
-    time.sleep(1)
+        light_state(lightpin1, True)
+        light_state(lightpin2, True)
+        
+        light_state(lightpin1, False)
+        light_state(lightpin2, False)
 
+    else:
+        if GPIO.getmode():
+            logging.info('Oprim instalatia')
+            off_everything([lightpin1, lightpin2])
+            GPIO.cleanup()
+        else:
+            logging.info('Inca nu!')
+        sleep(1800)
 
-GPIO.cleanup()
